90f41bbbf917a73505806461288ee8a17ffcbc14
Alexander Zutikov <zvlex.dev@gmail.com>
2018-11-01 23:52:33 +0400

    Go - dup and echo

--- ch1/echo1.go
+++ ch1/echo1.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
+	joinArgs()
+}
+
+func joinArgs() {
 	var s, sep string
 
 	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("Index: %d, Value: %s\n", i, os.Args[i])
+
 		s += sep + os.Args[i]
 		sep = " - "
 	}
